a64dfb1f4ec094426cbcc07514f6e9a65b76790d
Theodore Cross <theodorecross@Theodores-MacBook.local>
2019-10-13 15:11:08 -0500

    second commit :wq :wq!
    :q

--- GeoHero/VectorController.swift
+++ GeoHero/VectorController.swift
@@ -6,30 +6,207 @@
 //  Copyright © 2019 tedTosterone Enterprise. All rights reserved.
 //
 
-import UIKit
+import UIKit;
+import CoreLocation;
+import MapKit;
+
+struct Entity : Decodable {
+    let EntityID : Int
+    let EntityName : String
+    let EntityTypeID : Int
+    var EntityTypeName : String
+}
+
+struct Return : Decodable {
+    let CRUD : String
+}
 
 class VectorController: UIViewController {
-
+    
+    let locationManager = CLLocationManager();
+    
+    @IBOutlet weak var currentLocationLabel: UILabel!;
+    @IBOutlet weak var headerLabel: UILabel!;
+    
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    var options : [String] = [String]();
+    var ents : [Entity] = [Entity]();
+    var selectedType :String?;
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        super.viewDidLoad();
+        createLabels();
+        createPicker();
+        returnEntities();
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func createPicker(){
+        let typePicker = UIPickerView()
+        typePicker.delegate = self;
+        typePicker.dataSource = self;
+        typeTextField.inputView = typePicker;
+        fetchEntities();
+    }
+    
+    func createLabels(){
+        guard let long = locationManager.location?.coordinate.longitude, let lat = locationManager.location?.coordinate.latitude else { return };
+        headerLabel.text = "Add the following under my current coordinates.";
+        currentLocationLabel.text = "LONG: \(long) LAT: \(lat)";
+    }
+    
+    @IBAction func backtoView(_ sender: Any) {
+        dismiss(animated: true, completion: nil);
+    }
+    
+    func fetchEntities() {
+        let url = URL(string: "http://165.22.136.184:5000/coordinates/entityDetail/");
+        var arr = [String]();
+        
+        URLSession.shared.dataTask(with: url!) { (data, response, err) in
+            guard let data = data else { return }
+            
+            do {
+                let ents = try JSONDecoder().decode([Entity].self, from: data)
+                
+                for ent in ents {
+                    let str = ent.EntityTypeName + ": " + ent.EntityName;
+                    arr.append(str);
+                }
+                arr.insert("New Quest", at: 0)
+                self.populatePicker(arr: arr);
+            } catch let JSONerror {
+                print ("error parsing JSON", JSONerror)
+            }
+        }.resume()
+    }
+    
+    func returnEntities () {
+        let url = URL(string: "http://165.22.136.184:5000/coordinates/entityDetail/");
+        var array: [Entity] = [Entity]();
+        
+         URLSession.shared.dataTask(with: url!) { (data, response, err) in
+             guard let data = data else { return }
+             
+             do {
+                 array = try JSONDecoder().decode([Entity].self, from: data)
+                
+                self.populateEnts(array:array);
+             } catch let JSONerror {
+                 print ("error parsing JSON", JSONerror)
+             }
+         }.resume()
+    }
+    
+    func populateEnts(array : [Entity] ) {
+        ents = array;
+        print("ents in populate ents", ents);
+    }
+    
+    func populatePicker(arr: [String]) {
+        print("populate picker");
+        options = arr;
+    }
+    
+    @IBAction func addVector(_ sender: Any) {
+        if options.count != 0 && selectedType != nil {
+            print("thar she blows", selectedType!);
+            if selectedType! == "New Quest" {
+                print("segue to Edit Quests")
+            }
+            else {
+                print("add coordinate to database", selectedType!);
+                
+                var entID:Int = 0;
+                
+                let str = selectedType!.trimmingCharacters(in: .whitespacesAndNewlines)
+                let index = str.index(of: ":")!
+                let type = str.prefix(upTo: index)
+                let name = str.suffix(from: index).dropFirst().dropFirst();
+                
+                print(type)
+                print(name)
+                
+                for ent in ents {
+                    print(type, name, ent);
+                    if (ent.EntityTypeName == type && ent.EntityName == name){
+                        entID = ent.EntityID
+                        break;
+                    }
+                }
+                
+                //match name and get entityID
+                if (entID == 0){
+                    print ("we havnt found shit")
+                }
+                else {
+                    //put route
+                    print("success")
+                    
+                    guard let long = locationManager.location?.coordinate.longitude, let lat = locationManager.location?.coordinate.latitude else { return };
+                    headerLabel.text = "Add the following under my current coordinates.";
+                    currentLocationLabel.text = "LONG: \(long) LAT: \(lat)";
+                    
+                    let url = URL(string: "http://165.22.136.184:5000/CRUD/coordinate/C/null/\(long)/\(lat)/\(entID)");
+                    var array : [Return] = [Return]();
+                    
+                    URLSession.shared.dataTask(with: url!) { (data, response, err) in
+                        guard let data = data else { return }
+                        
+                        do {
+                            array = try JSONDecoder().decode([Return].self, from: data)
+                           
+                            print("created?", array)
+                        } catch let JSONerror {
+                            print ("error parsing JSON", JSONerror)
+                        }
+                    }.resume()
+                    
+                     performSegue(withIdentifier: "reloadVectors", sender: nil)
+                }
+            }
+        }
+        else {
+            print("its goddamn empty")
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "addVectorSegue"{
+            let nextVC = segue.destination as! VectorController;
+        }
+        else if segue.identifier == "reloadVectors" {
+            let nextVC = segue.destination as! ViewController;
+        }
     }
-    */
-
 }
+
+extension VectorController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in typePicker: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ typePicker: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return options.count
+    }
+    
+    func pickerView(_ typePicker: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return options[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedType = options[row];
+        typeTextField.text = selectedType
+        view.endEditing(true);
+    }
+    
+}
+
+
+
+
--- GeoHero/ViewController.swift
+++ GeoHero/ViewController.swift
@@ -6,20 +6,171 @@
 //  Copyright © 2019 tedTosterone Enterprise. All rights reserved.
 //
 
-import UIKit
+import UIKit;
+import CoreLocation;
+import MapKit;
+
+struct Vector : Decodable {
+    let CoordinateID : Int
+    let EntityID : Int
+    let EntityName : String
+    let EntityTypeName : String
+    let Longitude : Double
+    let Latitude : Double
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager();
+    let regionInMeters: Double = 10000;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        checkLocationServices()
+    }
+    
+    @IBAction func addVector(_ sender: Any) {
+        performSegue(withIdentifier: "addVectorSegue", sender: nil)
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, span: MKCoordinateSpanMake(0.003, 0.003))
+            mapView.setRegion(region, animated: true )
+        }
+    }
+    
+    func checkLocationAuthorization(){
+        switch CLLocationManager.authorizationStatus(){
+        case .authorizedWhenInUse:
+            //While using the app
+            mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+            getNearEntities()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            //Show user how to enable location services
+            break
+        case .notDetermined:
+            //ask permission
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //parental controls
+            break
+        case .authorizedAlways:
+            //run in background
+            break
+        }
+    }
+    
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            print ("User enable location services");
+        }
+    }
+    
+    func setupLocationManager(){
+        locationManager.delegate = self as CLLocationManagerDelegate;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func getNearEntities(){
+        guard let long = locationManager.location?.coordinate.longitude, let lat = locationManager.location?.coordinate.latitude else { return }
+        guard let url = URL(string: "http://165.22.136.184:5000/coordinates/entitiesClose/\(long)/\(lat)") else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            guard let data = data else { return }
+            
+            do {
+                let vectors = try JSONDecoder().decode([Vector].self, from: data)
+               
+                for vector in vectors {
+                    print(vector);
+                    let newVector = MKPointAnnotation();
+                    newVector.title = vector.EntityName
+                    newVector.subtitle = vector.EntityTypeName
+                    newVector.coordinate = CLLocationCoordinate2D(latitude: vector.Latitude, longitude: vector.Longitude)
+                    self.mapView.addAnnotation(newVector)
+                }
+            } catch let JSONerror {
+                print ("error parsing JSON entities", JSONerror)
+            }
+        }.resume()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addVectorSegue"{
+            _ = segue.destination as! VectorController;
+        }
     }
-
-
+//    @IBAction func tapAnnotation(_ sender: UITapGestureRecognizer) {
+//            let locationInView = sender.location(in: mapView)
+//            let locationOnMap = mapView.convert(locationInView, toCoordinateFrom : mapView)
+//            print ("Coord", locationOnMap)
+//
+//    }
+    
 }
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    //As user moves
+    func locationManaer(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, span: MKCoordinateSpanMake(0.003, 0.003))
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
+        
+    }
+}
+
+extension ViewController : MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is MKPointAnnotation else { print("no mkpointannotaions"); return nil }
+
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.rightCalloutAccessoryView = UIButton(type: .infoDark)
+            pinView!.pinTintColor = UIColor.red
+    }
+    else {
+        pinView!.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView){
+        print ("tapped on a pin");
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            if let vectorTitle = view.annotation?.title! {
+                print("encounter", vectorTitle)
+        }
+        }
+    }
+}
+
+
+
+
+
+
 
