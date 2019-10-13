d8878baad21958aee8716a4c73b78509b897440d
Theodore Cross <theodorecross@Theodores-MacBook.local>
2019-10-13 15:02:28 -0500

    first commit

---
+++ GeoHero/EncounterController.swift
@@ -0,0 +1,30 @@
+//
+//  EncounterController.swift
+//  GeoHero
+//
+//  Created by Theodore Cross on 10/13/19.
+//  Copyright © 2019 tedTosterone Enterprise. All rights reserved.
+//
+
+import UIKit
+
+class EncounterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GeoHero/VectorController.swift
@@ -0,0 +1,35 @@
+//
+//  VectorController.swift
+//  GeoHero
+//
+//  Created by Theodore Cross on 9/2/19.
+//  Copyright © 2019 tedTosterone Enterprise. All rights reserved.
+//
+
+import UIKit
+
+class VectorController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
